c7440ce0b4f0d90a91ef83d89bcfc6450b956c25
wenchangliu <wenchangliu@google.com>
2020-06-20 18:47:09 +0800

    munch: video: enbale adaptive b-frame capability
    'enc_default_bframe_count' set to 1.
    'enc_max_mbs_per_sec' set to max-hw-load.
    
    Bug: 156724722
    Test: run ECO experiments
    Change-Id: I884be887e9ae71bb9f8778f8bd0f5dd728d1ba7e

--- device.mk
+++ device.mk
@@ -249,7 +249,8 @@ endif
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
-    media_codecs_performance_c2.xml
+    media_codecs_performance_c2.xml \
+    video_system_specs.json
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
