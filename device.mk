c59ed923ac80934725bf30baa5180120cb50cc1b
Bruno Martins <bgcngm@gmail.com>
2023-11-13 17:41:11 +0000

    munch: Migrate to QTI health AIDL service
    QTI health AIDL HAL service enables suspend by default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Change-Id: Id90368ac64275120aa48b7865b70398c89a8c439
    
    The health AIDL HAL service is not allowed to read ro.charger.*
    system properties. This prop is deprecated.
    
    Change-Id: I320d2e2649663e5cd37ac5cdb1a1da677952c530
    
    Fix charger init trigger event after health AIDL move
    Change-Id: I58e7c493d3b300b1efb1685fba637fee6b56bec3

--- device.mk
+++ device.mk
@@ -238,8 +238,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1.vendor \
+    android.hardware.health-service.qti
 
 # HIDL
 PRODUCT_PACKAGES += \
