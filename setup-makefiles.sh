da619dbb9c26b4158790959ae07057158fe95b44
Tokito_to <RustedSilver11@proton.me>
2024-05-02 23:37:12 +0530

    Revert "munch: Exclude unneeded targets from tablet builds"
    This reverts commit 9d56f3d4424d2f8613851cd4d38f5df11c2d19ab.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -34,12 +34,5 @@ write_headers
 # The standard blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-# Exclude blobs from tablet builds
-printf '\n%s\n' 'ifneq ($(TARGET_IS_TABLET),true)' >> "$PRODUCTMK"
-
-write_makefiles "${MY_DIR}/proprietary-files-phone.txt" true
-
-printf '%s\n' 'endif' >> "$PRODUCTMK"
-
 # Finish
 write_footers
