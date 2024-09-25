8b339e18ac9f219f4fcc8f9b8345ca589343f808
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    munch: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- device.mk
+++ device.mk
@@ -552,7 +552,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_HAS_GADGET_HAL := true
 
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
