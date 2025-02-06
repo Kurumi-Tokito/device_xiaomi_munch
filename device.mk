89583e0073c48ae77690529c7306b089314b4bc5
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    munch: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device.mk
+++ device.mk
@@ -18,6 +18,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
