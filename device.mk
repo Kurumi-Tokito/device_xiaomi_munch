b9e537f397fcf78b993978b86da173bdb8eddf25
Tokito_to <RustedSilver11@proton.me>
2025-02-13 19:16:28 +0530

    munch: Nuke Thor
    - unusable due to upstream changes
    - qwant search engine which refuses to work
    - Android 15 support is not good

--- device.mk
+++ device.mk
@@ -441,10 +441,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
 endif
 
-# Thor Browser
-PRODUCT_PACKAGES += \
-    Thor
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
