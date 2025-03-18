c7da85872451b024e92311edb16984b843326b9c
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-18 18:05:43 +0800

    munch: Drop `TARGET_RECOVERY_DEVICE_MODULES`
    * It's deprecated on 15 QPR2
    * Building a library as module doesn't get it executed? and libinit
      still does its job in recovery mode without migrating this
    
    Change-Id: I7be000356fe7d7bd099615d334892d1686bece0e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,6 @@ ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_munch
-TARGET_RECOVERY_DEVICE_MODULES := libinit_munch
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
